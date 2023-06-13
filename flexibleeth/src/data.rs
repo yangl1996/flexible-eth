4e9c16de3e42ad077c3b8625a9e4fbd4aa7c21a5
Joachim Neu <email@joachim-neu.de>
2023-06-12 21:32:55 -0700

    feat: sync blocks

--- flexibleeth/src/data.rs
+++ flexibleeth/src/data.rs
@@ -1,44 +1,8 @@
-use ruint::Uint;
+// use ruint::{uint, Uint};
 use serde::{Deserialize, Serialize};
 use serde_aux::prelude::*;
 use std::error::Error;
 use std::fmt;
-use std::str::FromStr;
-
-type U256 = Uint<256, 4>;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct IdentifiedData<T> {
-    pub root: U256,
-    pub data: T,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Header {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub slot: usize,
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub proposer_index: usize,
-    pub parent_root: U256,
-    pub state_root: U256,
-    pub body_root: U256,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GetHeadersResponse {
-    pub data: Vec<GetHeadersResponseData>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GetHeadersResponseData {
-    pub root: U256,
-    pub header: GetHeadersResponseDataHeader,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GetHeadersResponseDataHeader {
-    pub message: Header,
-}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ResponseError {
@@ -54,3 +18,94 @@ impl fmt::Display for ResponseError {
 }
 
 impl Error for ResponseError {}
+
+// pub type Root = Uint<256, 4>;
+pub type Root = String;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct IdentifiedData<T> {
+    pub root: Root,
+    pub data: T,
+}
+
+// pub const HEADER_GENESIS_ROOT: Root =
+// uint!(0x4d611d5b93fdab69013a7f0a2f961caca0c853f87cfe9595fe50038163079360_U256);
+pub const HEADER_GENESIS_ROOT: &str =
+    "0x4d611d5b93fdab69013a7f0a2f961caca0c853f87cfe9595fe50038163079360";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Header {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub slot: usize,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub proposer_index: usize,
+    pub parent_root: Root,
+    pub state_root: Root,
+    pub body_root: Root,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetHeadersResponse {
+    pub data: Vec<GetHeadersResponseData>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetHeadersResponseData {
+    pub root: Root,
+    pub header: GetHeadersResponseDataHeader,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetHeadersResponseDataHeader {
+    pub message: Header,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Block {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub slot: usize,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub proposer_index: usize,
+    pub parent_root: Root,
+    pub state_root: Root,
+    pub body: BlockBody,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BlockBody {
+    pub attestations: Vec<Attestation>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Attestation {
+    pub aggregation_bits: String,
+    pub data: AttestationData,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AttestationData {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub slot: usize,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub index: usize,
+    pub beacon_block_root: Root,
+    pub source: Checkpoint,
+    pub target: Checkpoint,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Checkpoint {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub epoch: usize,
+    pub root: Root,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetBlockResponse {
+    pub data: GetBlockResponseData,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct GetBlockResponseData {
+    pub message: Block,
+}
--- flexibleeth/src/sync/mod.rs
+++ flexibleeth/src/sync/mod.rs
@@ -1,7 +1,8 @@
-use crate::data::{self, GetHeadersResponse, IdentifiedData, ResponseError};
+use crate::data::{self, GetBlockResponse, GetHeadersResponse, IdentifiedData, ResponseError};
+use bincode;
 use ratelimit::Ratelimiter;
 use reqwest;
-use rocksdb::{Options, DB};
+use rocksdb::DB;
 use serde_json;
 
 fn ratelimiter_wait(ratelimiter: &mut Ratelimiter) {
@@ -23,56 +24,47 @@ async fn get_headers(
         .text()
         .await?;
 
-    println!("{}", json_string);
     match serde_json::from_str::<GetHeadersResponse>(&json_string) {
         Ok(resp) => {
             let mut headers = Vec::new();
             for hdr in resp.data {
-                println!("{:?}", hdr.header.message);
-                //headers.push(IdentifiedData {
-                //    root: hdr["root"].as_str().unwrap().to_string(),
-                //     data: data::Header {
-                //         slot: hdr["header"].as_object().unwrap()["message"]
-                //             .as_object()
-                //             .unwrap()["slot"]
-                //             .as_u64()
-                //             .unwrap() as usize,
-                //         proposer_index: hdr["header"].as_object().unwrap()["message"]
-                //             .as_object()
-                //             .unwrap()["proposer_index"]
-                //             .as_u64()
-                //             .unwrap() as usize,
-                //         parent_root: hdr["header"].as_object().unwrap()["message"]
-                //             .as_object()
-                //             .unwrap()["parent_root"]
-                //             .as_str()
-                //             .unwrap()
-                //             .to_string(),
-                //         state_root: hdr["header"].as_object().unwrap()["message"]
-                //             .as_object()
-                //             .unwrap()["state_root"]
-                //             .as_str()
-                //             .unwrap()
-                //             .to_string(),
-                //         body_root: hdr["header"].as_object().unwrap()["message"]
-                //             .as_object()
-                //             .unwrap()["body_root"]
-                //             .as_str()
-                //             .unwrap()
-                //             .to_string(),
-                //     },
-                // });
+                headers.push(IdentifiedData {
+                    root: hdr.root,
+                    data: hdr.header.message,
+                });
             }
+            assert!(
+                slot != 0 || (headers.len() == 1 && headers[0].root == data::HEADER_GENESIS_ROOT)
+            );
             Ok(headers)
         }
         Err(_) => {
-            // It could either be that the slot is empty, or we encountered a server error.
-            // Figure it out by parsing the error message.
             let err = serde_json::from_str::<ResponseError>(&json_string)?;
-            match err.code {
-                404 => Ok(vec![]),
-                _ => Err(Box::new(err)),
-            }
+            Err(Box::new(err))
+        }
+    }
+}
+
+async fn get_block(
+    client: &mut reqwest::Client,
+    rpc_url: &str,
+    root: data::Root,
+) -> Result<data::IdentifiedData<data::Block>, Box<dyn std::error::Error>> {
+    let json_string = client
+        .get(format!("{}/eth/v2/beacon/blocks/{}", rpc_url, root))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    match serde_json::from_str::<GetBlockResponse>(&json_string) {
+        Ok(resp) => Ok(IdentifiedData {
+            root: root,
+            data: resp.data.message,
+        }),
+        Err(_) => {
+            let err = serde_json::from_str::<ResponseError>(&json_string)?;
+            Err(Box::new(err))
         }
     }
 }
@@ -86,21 +78,35 @@ pub async fn main(
     let db = DB::open_default(db_path)?;
     let mut rpc = reqwest::Client::new();
 
-    // slot number should be stored as u256 but u64 is fine
     let begin_slot = match db.get("sync_progress")? {
-        Some(serialized) => usize::from_le_bytes(serialized.try_into().unwrap()) + 1,
+        Some(serialized) => bincode::deserialize::<usize>(&serialized)? + 1,
         None => 0,
     };
+    log::info!("Syncing slots {}..{}", begin_slot, max_slot);
 
     for slot in begin_slot..max_slot {
+        log::debug!("Syncing slot {}", slot);
+
         ratelimiter_wait(&mut ratelimiter);
         let headers = get_headers(&mut rpc, &rpc_url, slot).await?;
-        println!("Headers: {:?}", headers);
-        // let body = reqwest::get(format!("{}/eth/v1/beacon/headers", https://www.rust-lang.org")
-        //     .await?
-        //     .text()
-        //     .await?;
-        db.put("sync_progress", slot.to_le_bytes())?;
+
+        let mut headers_roots = Vec::new();
+        for hdr in headers {
+            log::debug!("Header: {:?}", hdr);
+            db.put(format!("header_{}", hdr.root), bincode::serialize(&hdr)?)?;
+            headers_roots.push(hdr.root.clone());
+
+            ratelimiter_wait(&mut ratelimiter);
+            let blk = get_block(&mut rpc, &rpc_url, hdr.root).await?;
+            log::debug!("Block: {:?}", blk);
+            db.put(format!("block_{}", blk.root), bincode::serialize(&blk)?)?;
+        }
+        db.put(
+            format!("headers_for_slot_{}", slot),
+            bincode::serialize(&headers_roots)?,
+        )?;
+
+        db.put("sync_progress", bincode::serialize(&slot)?)?;
     }
 
     Ok(())
