28197af192d12c630e9cef8e65d0897b11bb5144
Joachim Neu <email@joachim-neu.de>
2023-07-06 00:35:09 -0700

    feat: cleanup experiment results

--- flexibleeth/run-example.sh
+++ flexibleeth/run-example.sh
@@ -10,6 +10,7 @@ MAXSLOT=$1
 
 cargo run --release -- -vv sync --rpc-url http://jneu:8Crd2iUnG0qg57M8RPcYKuPbZYZOwrLPLhHpTNvI@home.leiy.me:46235 --max-slot $MAXSLOT --rl-requests 1000
 
+
 mkdir output-example-$MAXSLOT
 
 cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.67 > output-example-$MAXSLOT/q67.txt
@@ -21,3 +22,12 @@ cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.97 > output-
 cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.98 > output-example-$MAXSLOT/q98.txt
 cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.99 > output-example-$MAXSLOT/q99.txt
 
+
+cat output-example-$MAXSLOT/q67.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q67.dat
+cat output-example-$MAXSLOT/q80.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q80.dat
+cat output-example-$MAXSLOT/q90.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q90.dat
+cat output-example-$MAXSLOT/q95.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q95.dat
+cat output-example-$MAXSLOT/q96.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q96.dat
+cat output-example-$MAXSLOT/q97.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q97.dat
+cat output-example-$MAXSLOT/q98.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q98.dat
+cat output-example-$MAXSLOT/q99.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q99.dat
