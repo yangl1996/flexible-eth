9ee5c093cdffe56a88a1ade04b4453de63c16a8d
Joachim Neu <jneu@stanford.edu>
2023-07-06 10:39:35 -0700

    feat: add db path to experiment script

--- flexibleeth/run-example.sh
+++ flexibleeth/run-example.sh
@@ -1,26 +1,27 @@
 #! /bin/sh
 
-if [ $# -lt 1 ]; then
-	echo "Usage: ./run-example.sh <MAXSLOT>"
+if [ $# -lt 2 ]; then
+	echo "Usage: ./run-example.sh <DBPATH> <MAXSLOT>"
     exit 1
 fi
 
-MAXSLOT=$1
+DBPATH=$1
+MAXSLOT=$2
 
 
-cargo run --release -- -vv sync --rpc-url http://jneu:8Crd2iUnG0qg57M8RPcYKuPbZYZOwrLPLhHpTNvI@home.leiy.me:46235 --max-slot $MAXSLOT --rl-requests 1000
+cargo run --release -- -vv sync --db-path $DBPATH --rpc-url http://jneu:8Crd2iUnG0qg57M8RPcYKuPbZYZOwrLPLhHpTNvI@home.leiy.me:46235 --max-slot $MAXSLOT --rl-requests 1000
 
 
 mkdir output-example-$MAXSLOT
 
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.67 > output-example-$MAXSLOT/q67.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.80 > output-example-$MAXSLOT/q80.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.90 > output-example-$MAXSLOT/q90.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.95 > output-example-$MAXSLOT/q95.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.96 > output-example-$MAXSLOT/q96.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.97 > output-example-$MAXSLOT/q97.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.98 > output-example-$MAXSLOT/q98.txt
-cargo run --release -- -vv conf-rule --max-slot $MAXSLOT --quorum 0.99 > output-example-$MAXSLOT/q99.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.67 > output-example-$MAXSLOT/q67.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.80 > output-example-$MAXSLOT/q80.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.90 > output-example-$MAXSLOT/q90.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.95 > output-example-$MAXSLOT/q95.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.96 > output-example-$MAXSLOT/q96.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.97 > output-example-$MAXSLOT/q97.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.98 > output-example-$MAXSLOT/q98.txt
+cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.99 > output-example-$MAXSLOT/q99.txt
 
 
 cat output-example-$MAXSLOT/q67.txt | awk -F'[= ]' '{ print "("$5", "$9")" }' > output-example-$MAXSLOT/q67.dat
