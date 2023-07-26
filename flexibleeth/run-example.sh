1a34b41164b1824d116cb9e92cba25394a851ff2
Lei Yang <lei@yangl1996.com>
2023-07-26 11:51:29 +0800

    update experiment script

--- flexibleeth/run-example.sh
+++ flexibleeth/run-example.sh
@@ -1,27 +1,27 @@
 #! /bin/bash -ve
 
 if [ $# -lt 2 ]; then
-	echo "Usage: ./run-example.sh <DBPATH> <MAXSLOT>"
+	echo "Usage: ./run-example.sh <DBPATH> <MINSLOT> <MAXSLOT>"
     exit 1
 fi
 
 DBPATH=$1
-MAXSLOT=$2
+MINSLOT=$2
+MAXSLOT=$3
 # BEACONAPIURL=http://flexeth:8Crd2iUnG0qg57M8RPcYKuPbZYZOwrLPLhHpTNvI@localhost:46235
 # BEACONAPIURL=http://flexeth:8Crd2iUnG0qg57M8RPcYKuPbZYZOwrLPLhHpTNvI@185.209.178.219:46235
 BEACONAPIURL=http://localhost:5052
 
+cargo run --release -- -vv sync --db-path $DBPATH --rpc-url $BEACONAPIURL --min-slot $MINSLOT --max-slot $MAXSLOT --rl-requests 1000
+mkdir output-example-$MINSLOT-$MAXSLOT || true
+cargo run --release -- -vv conf-rule --db-path $DBPATH --min-slot $MINSLOT --max-slot $MAXSLOT --quorum 0.67 --quorum 0.80 --quorum 0.90 --quorum 0.95 --quorum 0.96 --quorum 0.97 --quorum 0.98 --quorum 0.99 > output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt
 
-cargo run --release -- -vv sync --db-path $DBPATH --rpc-url $BEACONAPIURL --max-slot $MAXSLOT --rl-requests 1000
-mkdir output-example-$MAXSLOT || true
-cargo run --release -- -vv conf-rule --db-path $DBPATH --max-slot $MAXSLOT --quorum 0.67 --quorum 0.80 --quorum 0.90 --quorum 0.95 --quorum 0.96 --quorum 0.97 --quorum 0.98 --quorum 0.99 > output-example-$MAXSLOT/conf-rule-log.txt
-
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.67," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q67.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.8," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q80.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.9," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q90.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.95," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q95.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.96," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q96.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.97," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q97.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.98," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q98.txt
-cat output-example-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.99," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MAXSLOT/q99.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.67," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q67.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.8," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q80.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.9," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q90.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.95," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q95.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.96," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q96.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.97," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q97.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.98," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q98.txt
+cat output-example-$MINSLOT-$MAXSLOT/conf-rule-log.txt | grep "quorum: 0.99," | awk -F'[= ]' '{ print "("$3", "$11")" }' > output-example-$MINSLOT-$MAXSLOT/q99.txt
 
