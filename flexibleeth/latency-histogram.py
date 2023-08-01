9bf502617c5c63fc1feeb703b76efe920e92b160
Lei Yang <lei@yangl1996.com>
2023-08-01 15:11:46 -0400

    keep it simple and just use bins of width 1

--- flexibleeth/latency-histogram.py
+++ flexibleeth/latency-histogram.py
@@ -1,11 +1,11 @@
 import sys
 import re
-import argparse
+#import argparse
 import math
 
-argParser = argparse.ArgumentParser()
-argParser.add_argument("-n", help="number of bins", type=int, default=50)
-args = argParser.parse_args()
+#argParser = argparse.ArgumentParser()
+#argParser.add_argument("-n", help="number of bins", type=int, default=50)
+#args = argParser.parse_args()
 
 e = re.compile(r"t=([0-9]+) tip=([0-9]+),")
 
@@ -30,16 +30,13 @@ for line in sys.stdin:
                     max_d = latency
         confirmed_tip = tip
 
-if args.n >= max_d-min_d:
-    args.n = max_d-min_d
-bins = [0.0 for i in range(args.n)]
-width = (max_d-min_d)/args.n
+n_bins = max_d-min_d+1
+bins = [0.0 for i in range(n_bins)]
+width = 1
 
 for d in data:
-    idx = math.floor((d-min_d)/width)
-    if idx == args.n:
-        idx = args.n-1
+    idx = d-min_d
     bins[idx] += 1/len(data)/width
 
-for idx in range(args.n):
+for idx in range(n_bins):
     print(min_d+idx*width, min_d+idx*width+width, bins[idx])
